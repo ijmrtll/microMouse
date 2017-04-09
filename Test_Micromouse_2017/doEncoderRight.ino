b6e4251fbe6787f169f12fdd3332dcd785b6f2a2
Victor Figueroa <ijmrtll@gmail.com>
2017-04-08 22:22:43 -0700

    updated values of speeds

--- Test_Micromouse_2017/doEncoderRight.ino
+++ Test_Micromouse_2017/doEncoderRight.ino
@@ -1,4 +1,10 @@
 void doEncoderRight() {
+  /* If pinA and pinB are both high or both low, it is spinning
+     forward. If they're different, it's going backward.
+
+     For more information on speeding up this process, see
+     [Reference/PortManipulation], specifically the PIND register.
+  */
   if (digitalRead(encoderRightPinA) == digitalRead(encoderRightPinB)) {
     encoderRightPos++;
 
